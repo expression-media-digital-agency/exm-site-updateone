07dd8ba51bad8a88139904dc91ba27843ebb1dfc
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-01-07 13:27:15 +0100

    completed the contact page

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -8,6 +8,21 @@ const ContactForm = () => (
                             <h1 className="heading-one">
                                 Contact Information
                             </h1>
+
+                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/location.svg"/> Gibson House, No 12
+Flatten building, Ile Ife.</p>
+                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/message.svg"/> info@exmediaagency.com</p>
+                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/whatsapp.svg"/> +234(0) 81 4945 6123</p>
+
+                            <h2 className="heading-two">
+                                We are actively social
+                            </h2>
+                            
+                            <li className="icons">
+                                <span> <a href="https://www.facebook.com/exmdigitalagency/" target="_blank"><img className="img-fluid" src="/static/img/svg/facebook.svg" alt="facebookicon"/></a></span>
+                                <span> <a href="https://www.instagram.com/exmdigital/" target="_blank"><img className="img-fluid" src="/static/img/png/instagramwhite.png" alt="instagramicon"/></a></span>
+                                <span> <a href="https://twitter.com/exmdigital" target="_blank"><img className="img-fluid" src="/static/img/svg/twitter.svg" alt="twittericon" /></a></span>
+                            </li>
                         </div>
                         <div className="col-sm-7 form-right-side">
                             <form method="POST" action="https://formspree.io/exmdigitalagency@gmail.com">
