307af5c44bae80ae5280eb6f9c532fb24830ea2e
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2020-11-11 18:13:57 +0100

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -12,16 +12,11 @@ const Footer = () =>  (
                             <h3 className="footer-heading">Explore</h3>
                                 
                                 <li>
-                                    <Link href="/portfolio">
-                                        <a className="footer-text">Portfolio</a>
+                                    <Link href="/index">
+                                        <a className="footer-text">Home</a>
                                     </Link>
                                 </li>
-                                <li>
-                                    <Link href="/services">
-                                        <a className="footer-text">Terms of Service</a>
-                                    </Link>
-                                </li>
-                                <li>
+                             
                                     <Link href="/about">
                                         <a className="footer-text">About Us</a>
                                     </Link>
@@ -37,15 +32,18 @@ const Footer = () =>  (
                         <div className="col-sm-3">
                             <h3 className="footer-heading">Contact Us</h3>
                             <li className="footer-text">
-                                hi@expressionmedia.com
+                                exmdigitalagency@gmail.com
+                            </li>
+                                 <li className="footer-text">
+                                +(234)8169732091
                             </li>
                             <li className="footer-text">
                                 
                             </li>
                             <li className="icons">
-                                <span> <a href="https://www.facebook.com/exmdigitalagency/" target="_blank"><img src="/static/img/svg/facebookic.svg" alt="facebook"/></a></span>
-                                <span><a href="https://www.instagram.com/exmdigital/" target="_blank"><img src="/static/img/png/instagramwhite.png" alt="instagram"/></a></span>
-                                <span><a href="https://twitter.com/exmdigital" target="_blank"><img src="/static/img/svg/twitteric.svg" alt="twitter"/></a></span>
+                                <span> <a href="https://www.facebook.com/exmediadigital/" target="_blank"><img src="/static/img/svg/facebookic.svg" alt="facebook"/></a></span>
+                                <span><a href="https://www.instagram.com/exmediadigital/" target="_blank"><img src="/static/img/png/instagramwhite.png" alt="instagram"/></a></span>
+                                <span><a href="https://twitter.com/exmediadigital" target="_blank"><img src="/static/img/svg/twitteric.svg" alt="twitter"/></a></span>
                             </li>
                         </div>
 
@@ -58,7 +56,7 @@ const Footer = () =>  (
                     </div>
                     <div className="row">
                         <div className="col-sm-12 text-center copyright">
-                            <p className="footer-text ">Copyright 2019 (c) Expression Media Digital Agency</p>
+                            <p className="footer-text ">Copyright 2020 (c) Expression Media Digital Agency</p>
                         </div>
                     </div>
                 </div>
