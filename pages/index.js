7c051e96d71b6586f751cb7d0996e841a3fabb07
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-02-26 14:19:34 +0100

    New updates

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,11 @@ const Index = () => <Layout>
        <div className="row">
         <div className="col-sm-12 col-lg-7">
             <h1 className="header-one animated fadeIn delay-1s">
-            Grow Your Digital Presence with ExMedia Digital.
+            Grow Your Startup Revenue with ExMedia Digital.
             
             </h1>
  
-            <p className="small-text animated fadeIn delay-2s">Boost sales and increase profit.</p>
+            <p className="small-text animated fadeIn delay-2s">Generate and multiply Sales Online</p>
 
 
             <Link href="/contact">
@@ -51,22 +51,21 @@ const Index = () => <Layout>
                 <div className="card hvr-grow">
                     <img src="/static/img/svg/brand.svg" className="card-img-top" alt="branding picture"/>
                     <div className="card-body">
-                        <h3 className="card-title">Digital Marketing Strategy</h3>
-                        <p className="card-text">We create a high performing and measurable digital marketing plan that aligns with your overall marketing goals, and  walk you through the implementation process.</p>
-                    </div>
+                        <h3 className="card-title">Social Media Audit</h3>
+                        <p className="card-text">We review your startup metrics to assess growth, opportunities, and what should be done to generate and multiply your sales online.</div>
                 </div>
                 <div className="card hvr-grow">
                     <img src="/static/img/svg/marketing1.svg" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h3 className="card-title">Social Media Management</h3>
-                        <p className="card-text">Our team of highly skilled and experienced social media managers will handle your social media accounts with the right amount of attention while you focus on running your business.</p>
+                        <h3 className="card-title">Social Media Marketing</h3>
+                        <p className="card-text">Our team of highly skilled and experienced social media marketers will handle your social media accounts with cutting-edge professionalism to deliver the best results for your startup.</p>
                     </div>
                 </div>
                 <div className="card hvr-grow">
                     <img src="/static/img/svg/softdev1.svg" className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h3 className="card-title">Ads Campaign</h3>
-                        <p className="card-text">We put your business or brand in front of the right people, through carefully selected campaigns that connect you to the right audience and generate conversions.</p>
+                        <p className="card-text">We make your startup highly profitable through carefully created campaigns that connect you to the right audience and generate conversions.</p>
                     </div>
                 </div>
             </div>
@@ -87,7 +86,7 @@ const Index = () => <Layout>
             <div className="col-sm-4 text-center">
                 <img src="/static/img/svg/social-media 2.svg" className="img-fluid"/>
                 <p className="paragraph-one">
-                    Social Media Accounts <br /> Management
+                    Social Media Marketing <br />
                 </p>
             </div>
             <div className="col-sm-4 text-center">
@@ -99,7 +98,7 @@ const Index = () => <Layout>
             <div className="col-sm-4 text-center">
                 <img src="/static/img/svg/analysis 1.svg" className="img-fluid"/>
                 <p className="paragraph-one">
-                Social Media Analytics
+                Social Media Audit
                 </p>
             </div>
 
@@ -114,13 +113,13 @@ const Index = () => <Layout>
             <div className="col-sm-4 text-center">
                 <img src="/static/img/svg/contentM.svg"  className="img-fluid"/>
                 <p className="paragraph-one">
-                Copy Writing
+                Social Media Strategy
                 </p>
             </div>
             <div className="col-sm-4 text-center">
                 <img src="/static/img/svg/Cross-Platform.svg" className="img-fluid"/>
                 <p className="paragraph-one">
-                Cross-platform Advertising
+                 Advertising Campaigns
                 </p>
             </div>
 
