0a7c52026be8e74e706124c73815a8d532070768
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-03-01 10:35:42 +0100

    update5

--- components/NeedSolution/NeedSolution.js
+++ components/NeedSolution/NeedSolution.js
@@ -10,9 +10,9 @@ const NeedSolution = () => (
             </div> */}
             <div className="col-sm-8">
                 <h3 className="heading-three">Hello there! </h3> {/* Hi! I’m Anita.</h3> */}
-                <h1  className="heading-one">Need to Market Your Brand?</h1>
-                <h2 className="heading-two">Let's get started with you.</h2>
-                <p className="paragraph-one mb-5">Our team of creatives are ready to help, just a click away to get started.</p>
+                <h1  className="heading-one">Need to Grow Your Startup Revenue?</h1>
+                <h2 className="heading-two">Let's get started now.</h2>
+                <p className="paragraph-one mb-5">Our team of marketers are ready to achieve your goals, just a click away to get started.</p>
                 <Link href="/contact">
                     <a className="solution-btn hvr-grow-shadow">
                         Hire Us Now!
