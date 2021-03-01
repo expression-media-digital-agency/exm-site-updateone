0d0406f48a0e8cdc4cac017fee3ae14481aae2c8
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-03-01 10:53:20 +0100

    update7

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -10,17 +10,17 @@ const ContactForm = () => (
                             </h1>
 
                             {/* <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/location.svg"/> Gibson House, No 12 Flatten building, Ile Ife.</p> */}
-                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/message.svg"/> info@exmediaagency.com</p>
-                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/whatsapp.svg"/> +234(0) 81 4945 6123</p>
+                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/message.svg"/> exmdigitalagency@gmail.com</p>
+                            <p className="paragraph"><img className="img-fluid pr-3" src="/static/img/svg/whatsapp.svg"/> +(234)8169732091</p>
 
                             <h2 className="heading-two">
                                 We are actively social
                             </h2>
                             
                             <li className="icons">
-                                <span> <a href="https://www.facebook.com/exmdigitalagency/" target="_blank"><img className="img-fluid" src="/static/img/svg/facebook.svg" alt="facebookicon"/></a></span>
-                                <span> <a href="https://www.instagram.com/exmdigital/" target="_blank"><img className="img-fluid" src="/static/img/png/instagramwhite.png" alt="instagramicon"/></a></span>
-                                <span> <a href="https://twitter.com/exmdigital" target="_blank"><img className="img-fluid" src="/static/img/svg/twitter.svg" alt="twittericon" /></a></span>
+                                <span> <a href="https://www.facebook.com/exmediadigital/" target="_blank"><img className="img-fluid" src="/static/img/svg/facebook.svg" alt="facebookicon"/></a></span>
+                                <span> <a href="https://www.instagram.com/exmediadigital/" target="_blank"><img className="img-fluid" src="/static/img/png/instagramwhite.png" alt="instagramicon"/></a></span>
+                                <span> <a href="https://twitter.com/exmediadigital" target="_blank"><img className="img-fluid" src="/static/img/svg/twitter.svg" alt="twittericon" /></a></span>
                             </li>
                         </div>
                         <div className="col-sm-12 col-md-12 col-lg-7 form-right-side">
