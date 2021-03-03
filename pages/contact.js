5d6509fda6bc3205fb26a5b0f77cc2c0bc4f7b42
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-03-03 14:31:06 +0100

    update page

--- pages/contact.js
+++ pages/contact.js
@@ -15,22 +15,6 @@ const Academy = () => <Layout>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
 </Head>
-// <header className="contact-header">
-//     <div className="container">
-//        <div className="row">
-//             <div className=" col-sm-12 col-lg-4 center-pics">
-//             </div>
-//             <div className="col-sm-12 col-lg-2"></div>
-//             <div className="col-sm-12 col-lg-6">
-//                 <h1 className="heading-one animated fadeIn delay-2s">
-           
-//                 </h1>
-//             </div>
-
-
-//        </div>
-//     </div> 
-// </header>
 
     <ContactForm/>
 </Layout>;
