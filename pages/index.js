d33594b6b8d4c1b1106bbe30473cb084dc1a17ba
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-03-03 20:12:59 +0100

    update4google

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const Index = () => <Layout>
 <Head>
     <title>EXM | Home</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport"/>
+        <meta name="google-site-verification" content="kmbrx57bxpG7mnv9qSUcjLAbULlGQwDRthw8Zid_gyY" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"></link>
     <link href="https://fonts.googleapis.com/css?family=Be+Vietnam:400,800&display=swap" rel="stylesheet"></link>
