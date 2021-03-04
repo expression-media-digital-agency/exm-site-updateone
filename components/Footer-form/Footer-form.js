897374f8100ca9b7a0c09d9aedc1655eeb1692f3
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-03-04 14:30:25 +0100

    footer update

--- components/Footer-form/Footer-form.js
+++ components/Footer-form/Footer-form.js
@@ -6,10 +6,10 @@ const FooterForm = () =>  (
         <div>
             <form className="footer-form" method="POST" action="https://formspree.io/f/meqpbnzw">
                 <div className="form-group">
-                    <input type="email" className="form-control input" id="email" placeholder="Your email..."/>
+                    <input type="name" className="form-control input" id="name" placeholder="Your name."/>
                 </div>
                 <div className="form-group">
-                <textarea className="form-control textarea" name="message" rows="5" placeholder="Your message here..."></textarea>
+                <input type="email" className="form-control input" id="email" placeholder="Your email."/>
                 </div>
                 <div className="float-right">
                     <button type="submit" className="btn btn-footer hvr-grow-shadow">Send</button>
